1add505a3e9e06d8fb2a5fac1053eca1b3436f7d
Trainee <Trainee@admin-PC>
2015-01-07 11:07:46 +0530

    after test empty methods

--- srustiapp/src/main/test/com/srusti/dao/test/CustomerDaoTest.java
+++ srustiapp/src/main/test/com/srusti/dao/test/CustomerDaoTest.java
@@ -1,6 +1,37 @@
 package com.srusti.dao.test;
 
+import org.junit.Before;
+import org.junit.Test;
+
 public class CustomerDaoTest 
 {
+	@Before
+	public void setUp()
+	{
+		
+	}
 	
+	@Test
+	public void save()
+	{
+		
+	}
+	
+	@Test
+	public void getCustomer()
+	{
+		
+	}
+	
+	@Test
+	public void getCustomersList()
+	{
+		
+	}
+	
+	@Test
+	public void remove()
+	{
+		
+	}
 }
