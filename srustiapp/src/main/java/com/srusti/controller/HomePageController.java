d95a6fd944f28ba8a0409a6bff0f51879142f6f8
vamsichadaram <vamsi.chadaram@gmail.com>
2015-01-13 22:32:06 +0530

    after spring dependecies

---
+++ srustiapp/src/main/java/com/srusti/controller/HomePageController.java
@@ -0,0 +1,18 @@
+package com.srusti.controller;
+
+import org.apache.log4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/home")
+public class HomePageController 
+{
+	private static final Logger LOG= Logger.getLogger(HomePageController.class);
+	@RequestMapping("/sample")
+	public String homePage()
+	{
+		LOG.info("returning to home page");
+		return "home";
+	}
+}
