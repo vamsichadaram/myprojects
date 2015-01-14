c8c0c77e62320f29d21167028fd9ffa3cd381a7b
Trainee <Trainee@acaluk.acalpreprod1>
2015-01-14 16:53:31 +0530

    Media model

--- srustiapp/src/main/java/com/srusti/model/EmailModel.java
+++ srustiapp/src/main/java/com/srusti/model/EmailModel.java
@@ -8,47 +8,5 @@ public class EmailModel
 	private String cc;
 	private String subject;
 	private String message;
-	public String getToAddress() {
-		return toAddress;
-	}
-	public void setToAddress(String toAddress) {
-		this.toAddress = toAddress;
-	}
-	public String getFromAddress() {
-		return fromAddress;
-	}
-	public void setFromAddress(String fromAddress) {
-		this.fromAddress = fromAddress;
-	}
-	public String getBcc() {
-		return bcc;
-	}
-	public void setBcc(String bcc) {
-		this.bcc = bcc;
-	}
-	public String getCc() {
-		return cc;
-	}
-	public void setCc(String cc) {
-		this.cc = cc;
-	}
-	public String getSubject() {
-		return subject;
-	}
-	public void setSubject(String subject) {
-		this.subject = subject;
-	}
-	public String getMessage() {
-		return message;
-	}
-	public void setMessage(String message) {
-		this.message = message;
-	}
 	
-	@Override
-	public String toString() {
-		return "EmailModel [toAddress=" + toAddress + ", fromAddress="
-				+ fromAddress + ", bcc=" + bcc + ", cc=" + cc + ", subject="
-				+ subject + ", message=" + message + "]";
-	}
 }
---
+++ srustiapp/src/main/java/com/srusti/model/MediaModel.java
@@ -0,0 +1,68 @@
+package com.srusti.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="media")
+public class MediaModel 
+{
+	@Id
+	@GeneratedValue
+	private String id;
+	@Column
+	private String name;
+	@Column
+	private String path;
+	@Column
+	private String title;
+	@Column
+	private String alt;
+	@Column
+	private String decription;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAlt() {
+		return alt;
+	}
+	public void setAlt(String alt) {
+		this.alt = alt;
+	}
+	public String getDecription() {
+		return decription;
+	}
+	public void setDecription(String decription) {
+		this.decription = decription;
+	}
+	@Override
+	public String toString() {
+		return "MediaModel [id=" + id + ", name=" + name + ", path=" + path
+				+ ", title=" + title + ", alt=" + alt + ", decription="
+				+ decription + "]";
+	}
+}
