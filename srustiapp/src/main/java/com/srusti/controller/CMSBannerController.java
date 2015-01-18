b47200be7e7a287fc7a52da24677a59bdf6baaf0
vamsichadaram <vamsi.chadaram@gmail.com>
2015-01-18 14:04:17 +0530

    controllers

--- srustiapp/src/main/java/com/srusti/controller/CMSBannerController.java
+++ srustiapp/src/main/java/com/srusti/controller/CMSBannerController.java
@@ -1,9 +1,13 @@
 package com.srusti.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.srusti.model.components.BannerComponent;
 import com.srusti.service.BannerService;
 
 @Controller
@@ -12,4 +16,25 @@ public class CMSBannerController
 {
 	@Autowired
 	private BannerService service;
+	
+	@RequestMapping(value="/save", method=RequestMethod.POST)
+	public void save(BannerComponent banner)
+	{
+		service.save(banner);
+	}
+	@RequestMapping("/get")
+	public void get(int id)
+	{
+		service.get(id);
+	}
+	@RequestMapping("/remove")
+	public void remove(int id)
+	{
+		service.remove(id);
+	}
+	@RequestMapping("/list")
+	public List<BannerComponent> list()
+	{
+		return service.list();
+	}
 }
--- srustiapp/src/main/java/com/srusti/controller/CMSLinkController.java
+++ srustiapp/src/main/java/com/srusti/controller/CMSLinkController.java
@@ -1,9 +1,13 @@
 package com.srusti.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.srusti.model.components.LinkComponent;
 import com.srusti.service.LinkService;
 
 @Controller
@@ -12,4 +16,25 @@ public class CMSLinkController
 {
 	@Autowired
 	private LinkService service;
+	
+	@RequestMapping(value="/save", method=RequestMethod.POST)
+	public void save(LinkComponent link)
+	{
+		service.save(link);
+	}
+	@RequestMapping("/get")
+	public LinkComponent get(int id)
+	{
+		return service.get(id);
+	}
+	@RequestMapping("/remove")
+	public void remove(int id)
+	{
+		service.remove(id);
+	}
+	@RequestMapping("/list")
+	public List<LinkComponent> list()
+	{
+		return service.list();
+	}
 }
