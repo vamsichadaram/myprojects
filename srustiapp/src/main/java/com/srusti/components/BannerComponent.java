4653100f66816671221f48356eb320b54d68be67
vamsichadaram <vamsi.chadaram@gmail.com>
2015-01-18 10:04:58 +0530

    components

---
+++ srustiapp/src/main/java/com/srusti/components/BannerComponent.java
@@ -0,0 +1,21 @@
+package com.srusti.components;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="banners")
+public class BannerComponent 
+{
+	@Id
+	@GeneratedValue
+	private int id;
+	@Column
+	private String name;
+	@Column
+	private String decription;
+	
+}
