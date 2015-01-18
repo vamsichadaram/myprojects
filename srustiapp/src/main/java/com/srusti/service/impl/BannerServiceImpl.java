5270a7e968e97e7dcd88068fd66e5caa77bd4563
vamsichadaram <vamsi.chadaram@gmail.com>
2015-01-18 13:13:11 +0530

    commit

--- srustiapp/src/main/java/com/srusti/service/impl/BannerServiceImpl.java
+++ srustiapp/src/main/java/com/srusti/service/impl/BannerServiceImpl.java
@@ -2,30 +2,37 @@ package com.srusti.service.impl;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.srusti.dao.BannerDao;
 import com.srusti.model.components.BannerComponent;
 import com.srusti.service.BannerService;
 
+@Repository("bannerService")
 public class BannerServiceImpl implements BannerService
 {
-
-	public void save(BannerComponent banner) {
-		// TODO Auto-generated method stub
-		
+	@Autowired
+	private BannerDao dao;
+	
+	public void save(BannerComponent banner) 
+	{
+		dao.save(banner);
 	}
 
-	public BannerComponent get(int id) {
-		// TODO Auto-generated method stub
-		return null;
+	public BannerComponent get(int id) 
+	{
+		return dao.get(id);
 	}
 
-	public List<BannerComponent> list() {
-		// TODO Auto-generated method stub
-		return null;
+	public List<BannerComponent> list() 
+	{
+		return dao.list();
 	}
 
-	public void remove(int id) {
-		// TODO Auto-generated method stub
-		
+	public void remove(int id) 
+	{
+		dao.remove(id);
 	}
 	
 }
